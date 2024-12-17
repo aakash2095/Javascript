b15c54f9736b8ad240661065988e288aba033d84
Aakash Soni <soniaakash5336@gmail.com>
2024-12-17 17:13:27 +0530

    update

---
+++ practice.js
@@ -0,0 +1,13 @@
+const a=2;
+function square(n){
+    const ans = n * n;
+    return ans;
+    const a=10
+}
+
+const square_2 = square(2);
+const square_a = square(a);
+console.log('inside',a)
+
+// Execution context
+//global execution context 
