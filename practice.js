2b7a28c71941560c54e0edccb162b06c2f5f43a2
Aakash Soni <soniaakash5336@gmail.com>
2024-12-18 17:14:48 +0530

    update

--- practice.js
+++ practice.js
@@ -1,16 +1,46 @@
-{
-    const a=2;
-    function square(n){
-        const ans = n * n;
-        return ans;
-        const a=10
-    }
+// {
+//     const a=2;
+//     function square(n){
+//         const ans = n * n;
+//         return ans;
+//         const a=10
+//     }
 
-    const square_2 = square(2);
-    const square_a = square(a);
-    console.log('inside',a)
+//     const square_2 = square(2);
+//     const square_a = square(a);
+//     console.log('inside',a)
+// }
+
+// console.log(a)
+// // Execution context
+// //global execution context 
+
+
+// function x(params) {
+    
+// }
+
+// function y(params) {
+    
+// }
+
+
+// x(y);  //y is callback function 
+
+function perform(params) {
+    document.getElementById('clickme')
+    .addEventListener('click',function() {
+        params()
+        var p = document.createElement('p');
+        p.textContent = 'Paragraph Created';
+        p.id ='new-paragraph';
+        document.body.appendChild(p)
+        
+    });
 }
 
-console.log(a)
-// Execution context
-//global execution context 
+function x() {
+    console.log('i am clicked');
+}
+
+perform(x)
