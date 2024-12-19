cde554af6b4311681e49071359d0e621fdbe89b8
Aakash Soni <soniaakash5336@gmail.com>
2024-12-19 17:33:46 +0530

    update

--- practice.js
+++ practice.js
@@ -27,20 +27,39 @@
 
 // x(y);  //y is callback function 
 
-function perform(params) {
-    document.getElementById('clickme')
-    .addEventListener('click',function() {
-        params()
-        var p = document.createElement('p');
-        p.textContent = 'Paragraph Created';
-        p.id ='new-paragraph';
-        document.body.appendChild(p)
+// function perform(params) {
+//     document.getElementById('clickme')
+//     .addEventListener('click',function() {
+//         params()
+//         var p = document.createElement('p');
+//         p.textContent = 'Paragraph Created';
+//         p.id ='new-paragraph';
+//         document.body.appendChild(p)
         
-    });
+//     });
+// }
+
+// function x() {
+//     console.log('i am clicked');
+// }
+
+// perform(x)
+
+// for (i; i <= 5; i++) 
+// {
+//   document.write( i + "<br>");
+// }
+    
+var i= 1
+
+for (i;i <=5;i++) {
+
+    document.write(i  + '<br>');
+    console.log(i);
+
 }
 
-function x() {
-    console.log('i am clicked');
-}
-
-perform(x)
+setTimeout(()=>{
+    console.log(i);
+    
+},i*1000)
