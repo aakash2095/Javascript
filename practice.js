1e08cd7c070b2093355da645b8afdf8fdca6d542
Aakash Soni <soniaakash5336@gmail.com>
2024-12-20 17:10:20 +0530

    update

--- practice.js
+++ practice.js
@@ -27,6 +27,8 @@
 
 // x(y);  //y is callback function 
 
+
+
 // function perform(params) {
 //     document.getElementById('clickme')
 //     .addEventListener('click',function() {
@@ -45,21 +47,93 @@
 
 // perform(x)
 
+
+
+
+
+
 // for (i; i <= 5; i++) 
 // {
 //   document.write( i + "<br>");
 // }
     
-var i= 1
+// var i= 1
 
-for (i;i <=5;i++) {
+// for (i;i <=5;i++) {
 
-    document.write(i  + '<br>');
-    console.log(i);
+//     document.write(i  + '<br>');
+//     console.log(i);
 
+// }
+
+
+
+
+
+// var i= [1,3,4,2];
+
+
+// setTimeout(()=>{
+//     console.log(i);
+    
+// },i*1000)
+
+// let num = [1, 3, 4, 2,'hello'];
+
+// for (let i of num) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, i * 1000);
+// }
+
+
+// let i = [1,3,2,4];
+// for(i=1;i<4;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },i*1000);
+// }
+
+//    Promise
+
+const cart =['shoe','tshirt','jeans']
+
+const pr = createorder(cart).then(
+   // makepayment(orderid)
+   function (orderid){
+    console.log('orderid is',orderid);
+    return makepayment(orderid)
+   }
+)
+.then(
+    function (paymentstatus) {
+        return placeorder(paymentstatus)
+    }
+)
+.catch(function(err){
+    console.log(err.message)
+})
+
+function createorder(cart){
+    return new Promise(function(resolve, reject) {
+        let orderid='1';
+        resolve('order created')
+    })
 }
 
-setTimeout(()=>{
-    console.log(i);
-    
-},i*1000)
+
+function makepayment(orderid){
+    return new Promise(function(resolve, reject) {
+        let paymentstatus=true;
+        if (paymentstatus){
+            resolve(paymentstatus)
+        }
+        else{
+            const err = Error('Payment Failed')
+            reject(err)
+            
+        }
+    })
+}
+
+function
